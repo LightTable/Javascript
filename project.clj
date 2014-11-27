23cc4c46075b98db355351a37f181ffb657030aa
Gabriel Horner <gabriel.horner@gmail.com>
2014-11-27 01:53:47 -0500

    Bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/javascript "0.0.6"
+(defproject com.lighttable/javascript "0.1.0"
   :description "Javascript plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
