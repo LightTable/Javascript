35018077276adc4319acd7bc696fa4be38a82299
Gabriel Horner <gabriel.horner@gmail.com>
2014-12-01 17:26:31 -0500

    Bump to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.1
+* Fix shebang parsing
+
 ## 0.1.0
 * Fix console.log and on-out behavior
 * Upgrade to acorn 0.9.0
--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/javascript "0.1.0"
+(defproject com.lighttable/javascript "0.1.1"
   :description "Javascript plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
